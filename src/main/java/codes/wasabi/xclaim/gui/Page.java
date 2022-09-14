f879ae7c67d2852f3c3a73bf9c32bf840537c139
BuildTools <unconfigured@null.spigotmc.org>
2022-09-14 17:33:34 -0400

    Converted BossBar to action bar, 1.8 compatibility

--- src/main/java/codes/wasabi/xclaim/gui/Page.java
+++ src/main/java/codes/wasabi/xclaim/gui/Page.java
@@ -2,11 +2,11 @@ package codes.wasabi.xclaim.gui;
 
 import codes.wasabi.xclaim.XClaim;
 import codes.wasabi.xclaim.platform.Platform;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
-import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -16,13 +16,11 @@ import org.jetbrains.annotations.Nullable;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
-import static codes.wasabi.xclaim.util.StringUtil.repeatString;
-
 public abstract class Page {
 
     private final GUIHandler parent;
     protected boolean suspended = false;
-    private BossBar awaitBar;
+    private String awaitPrompt;
     private Consumer<String> awaitCallback;
     private boolean awaiting = false;
     public Page(@NotNull GUIHandler parent) {
@@ -36,8 +34,6 @@ public abstract class Page {
     public abstract void onEnter();
     public void onExit() {
         if (awaiting) {
-            awaitBar.setVisible(false);
-            awaitBar.removeAll();
             awaitCallback = ((String s) -> {});
         }
         awaiting = false;
@@ -57,21 +53,23 @@ public abstract class Page {
     }
 
     public void onTick() {
+        if (awaiting) {
+            Audience audience = Platform.getAdventure().player(getTarget());
+            audience.sendActionBar(Component.text(awaitPrompt).color(NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false));
+        }
     }
 
     protected final void prompt(@NotNull String prompt, @NotNull Consumer<String> callback) {
         boolean ticking = parent.getShouldTick();
         parent.setShouldTick(true);
         suspend();
-        awaitBar = Bukkit.createBossBar(prompt, BarColor.WHITE, BarStyle.SOLID);
-        awaitBar.addPlayer(getTarget());
-        awaitBar.setVisible(true);
-        awaitBar.setProgress(1d);
+        Player target = getTarget();
+        target.playSound(target.getLocation(), Platform.get().getExpSound(), 1f, 1f);
+        awaitPrompt = prompt;
         awaitCallback = ((String s) -> {
+            awaiting = false;
             parent.setShouldTick(ticking);
             Bukkit.getScheduler().runTask(XClaim.instance, () -> {
-                awaitBar.setVisible(false);
-                awaitBar.removeAll();
                 unsuspend();
                 callback.accept(s);
             });
